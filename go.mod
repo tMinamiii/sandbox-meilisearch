094756377a2cae738d7b0c99566ae6ce77d1765f
tMinamiii <otherthan.new@gmail.com>
2022-09-05 23:51:55 +0900

    search cmd

--- go.mod
+++ go.mod
@@ -3,17 +3,19 @@ module sandbox-meilisearch
 go 1.19
 
 require (
+	github.com/meilisearch/meilisearch-go v0.20.1
+	github.com/urfave/cli/v2 v2.14.0
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.6 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/meilisearch/meilisearch-go v0.20.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.14.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.37.1-0.20220607072126-8a320890c08d // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,90 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/meilisearch/meilisearch-go"
 	"github.com/urfave/cli/v2"
 )
 
+// IndexName is meilisearch index name
+const (
+	IndexName = "itnews"
+	Q         = "q"
+	Limit     = "limit"
+	Offset    = "offset"
+)
+
+type SearchParams struct {
+	q      string
+	limit  int64
+	offset int64
+}
+
+var client = func() *meilisearch.Client {
+	c := meilisearch.NewClient(meilisearch.ClientConfig{
+		Host:   "http://localhost",
+		APIKey: "MASTER_KEY",
+	})
+	return c
+}()
+
 func main() {
 	app := &cli.App{
+		Name: "meilisearch-cli",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:  "lang",
-				Value: "english",
-				Usage: "language for the greeting",
+				Name:  Q,
+				Usage: "search query",
+			},
+			&cli.Int64Flag{
+				Name:  Limit,
+				Usage: "hits limit",
+				Value: 20,
+			},
+			&cli.Int64Flag{
+				Name:  Offset,
+				Usage: "offset",
+				Value: 0,
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			name := "meilisearch"
-			if cCtx.NArg() > 0 {
-				name = cCtx.Args().Get(0)
+			q := cCtx.String(Q)
+			limit := cCtx.Int64(Limit)
+			offset := cCtx.Int64(Offset)
+			if q == "" {
+				return nil
 			}
-			if cCtx.String("lang") == "spanish" {
-				fmt.Println("Hola", name)
-			} else {
-				fmt.Println("Hello", name)
+			req := &meilisearch.SearchRequest{
+				Limit:  limit,
+				Offset: offset,
 			}
-			return nil
+			return run(q, req)
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run(q string, req *meilisearch.SearchRequest) error {
+	resp, err := client.Index(IndexName).Search(q, req)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	bjson, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(bjson))
+
+	return nil
+
 }
